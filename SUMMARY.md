f763e162bbc931e3e5a601937ab4ae354a0b6afd
Feng Hao <howiefh@gmail.com>
2015-10-17 12:43:27 +0800

    Initial commit

---
+++ SUMMARY.md
@@ -0,0 +1,7 @@
+# Summary
+
+This is the summary of my book.
+
+* [section 1](section1/README.md)
+    * [example 1](section1/example1.md)
+* [section 2](section2/README.md)
---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+deployDir=".deploy"
+ignoreDeleteFiles=".git\|.gitignore"
+repoUrl="https://github.com/howiefh/knowledge-summary.git"
+repoBranch="gh-pages"
+
+# 初始化发布书籍的目录
+if [ ! -d $deployDir ]; then
+    mkdir $deployDir
+    cd $deployDir
+    echo "deploy.sh" >> .gitignore
+    git init
+    git checkout -b $repoBranch
+    git add -A
+    git commit -m "First commit"
+    cd ..
+fi
+
+cd $deployDir
+# 删除除隐藏文件(.git和.gitignore)之外的其它文件
+ls | grep -v $ignoreDeleteFiles | xargs rm -rf
+cd ..
+
+gitbook build
+cp -r _book/* $deployDir
+cd $deployDir
+git add -A
+git commit -m "Book updated $(date "+%Y-%m-%d %H:%M:%S")"
+git push -u $repoUrl "HEAD:"${repoBranch} --force
